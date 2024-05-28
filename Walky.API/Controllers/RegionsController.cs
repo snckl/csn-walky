ec87ebc89d48e82921a0b55caf1e533c509c4729
Sinan <sinan.cakal00@gmail.com>
2024-05-28 20:18:47 +0300

    Basic crud for region

---
+++ Walky.API/Controllers/RegionsController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Walky.API.Data;
+using Walky.API.Models.Domain;
+using Walky.API.Models.DTO;
+
+namespace Walky.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly WalkyDbContext dbContext;
+
+        public RegionsController(WalkyDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+
+            var regionsDto = new List<RegionDto>();
+            foreach (var item in regions)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = item.Id,
+                    Code = item.Code,
+                    Name = item.Name,
+                    RegionImageUrl = item.RegionImageUrl
+                });   
+            }
+
+            return Ok(regionsDto);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if(region == null)
+            {
+                return NotFound();
+            }
+            var regionsDto = new RegionDto()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return Ok(regionsDto);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateRegionDto createRegionDto)
+        {
+            var region =  new Region()
+            {
+                Code = createRegionDto.Code,
+                Name = createRegionDto.Name,
+                RegionImageUrl = createRegionDto.RegionImageUrl
+            };
+            dbContext.Regions.Add(region);
+            dbContext.SaveChanges();
+
+            var regionDto = new RegionDto()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById),new { id = regionDto.Id},regionDto);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionDto updateRegionDto)
+        {
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            region.Code = updateRegionDto.Code;
+            region.Name = updateRegionDto.Name;
+            region.RegionImageUrl = updateRegionDto.RegionImageUrl;
+
+            dbContext.SaveChanges();
+
+            var regionDto = new RegionDto()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if(region == null)
+            {
+                return NotFound();
+            }
+
+            dbContext.Remove(region);
+            dbContext.SaveChanges();
+
+            return Ok();
+
+        }
+    }
+}
---
+++ Walky.API/Models/DTO/UpdateRegionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Walky.API.Models.DTO
+{
+    public class UpdateRegionDto
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
