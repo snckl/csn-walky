368f15fe2e0d57dc06189be29a7a9949ae724565
Sinan <sinan.cakal00@gmail.com>
2024-05-28 17:34:38 +0300

    Added Models

---
+++ Walky.API/Models/Domain/Walk.cs
@@ -0,0 +1,20 @@
+﻿namespace Walky.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkIamgeUrl { get; set; }
+        public Guid DifficultyId { get; set; }
+        public Guid RegionId { get; set; }
+
+
+
+        // Navigation Properties
+        public Difficulty Difficulty { get; set; }
+        public Region Region { get; set; }
+
+    }
+}
