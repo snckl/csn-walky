ea83500857f09e87cad243d248afab3010332cec
Sinan <sinan.cakal00@gmail.com>
2024-06-24 15:15:41 +0300

    Seeded auth with roles

--- Walky.API/Data/WalkyAuthDbContext.cs
+++ Walky.API/Data/WalkyAuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Walky.API.Data
@@ -8,6 +9,36 @@ namespace Walky.API.Data
         public WalkyAuthDbContext(DbContextOptions<WalkyAuthDbContext> options) : base(options)
         {
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "c47a9ff9-e60f-439d-915b-6539abd14cbc";
+            var writerRoleId = "2288c580-ab12-4317-9ebb-d486bab44154";
+
+            var roles = new List<IdentityRole>()
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+
+
+            builder.Entity<IdentityRole>().HasData(roles);
+
         }
     }
 }
