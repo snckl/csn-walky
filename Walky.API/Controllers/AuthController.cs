59947130aa9dba6e340fa9e9da559c2cf4d8be95
Sinan <sinan.cakal00@gmail.com>
2024-06-24 15:46:05 +0300

    Added auth controller and register endpoint

---
+++ Walky.API/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Walky.API.Models.DTO;
+
+namespace Walky.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this._userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerDto.Username,
+                Email = registerDto.Email
+            };
+
+            var result = await _userManager.CreateAsync(identityUser, registerDto.Password);
+
+            if (result.Succeeded)
+            {
+                if (registerDto.Roles != null)
+                {
+                    result = await _userManager.AddToRolesAsync(identityUser, registerDto.Roles);
+                    if (result.Succeeded)
+                    {
+                        return Ok("User is registered! Please log in.");
+                    }
+                } 
+      
+            }
+
+            return BadRequest("Something went wrong!");
+        }
+    }
+}
---
+++ Walky.API/Models/DTO/RegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Walky.API.Models.DTO
+{
+    public class RegisterDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string[] Roles { get; set; }
+    }
+}
